6f651eabe3bd0011b2ffb3aa150eea0473942303
Пан Микола <nickkolomietsdooor@gmail.com>
2023-01-28 07:42:59 +0200

    refactoring

--- projects/project1/js/moving_points.js
+++ projects/project1/js/moving_points.js
@@ -1,45 +1,113 @@
+var canvas_width = 1536;
+var canvas_height = 1000;
+var zoom_scale_x = 1;
+var zoom_scale_y = 1;
+
+var edge_rad_standart = 40;
+var edges_rad_max;
+
+var edges_rad_canvas_1;
+var edges_rad_canvas_2;
+
+var render = 1000.0/30.0; //fps
 var Engine = Matter.Engine,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
     Composite = Matter.Composite;
 
-const canvas = document.getElementById("canvas_1");
-const ctx = canvas.getContext("2d");
-let edge_rad = 10; 
-let speed = 5;
-let xMax = canvas.width = window.innerWidth * 0.55;
-let yMax = canvas.height = window.innerHeight * 0.65;
-let voronoi_on = true;
 
 var engine = Engine.create();
+engine.world.gravity.y = 0; 
 
-function start(){
-    var ground = Bodies.rectangle(0, canvas.height + 10, 5000, 20, { isStatic: true });
+let speed = 5;
+let voronoi_on = true;
+
+
+var edges_positions_canvas_1 = [];
+var edges_canvas_2 = [];
+var edges_bodies_canvas_1 = [];
+
+var canvas_1 = document.getElementById("canvas_1");
+var canvas_2 = document.getElementById("canvas_2");
+const ctx_1 = canvas_1.getContext("2d");
+const ctx_2 = canvas_2.getContext("2d");
+
+var xMax;
+var yMax;
+
+function min(x, y){
+    if (x < y){return x;}
+    else{return y;}
+}
+
+window.onload = function(){
+
+    resizeCanvas(canvas_1);
+    resizeCanvas(canvas_2);
+    
+    spawn_walls();
+    
+    //canvas.onmousedown = function(e) { onCanvasMouseDown(canvas, e); };
+    //canvas.onmousemove = function(e) { onCanvasMouseMove(canvas, e); };
+    //canvas.onmouseup = function(e) { onCanvasMouseUp(canvas, e); };
+    //canvas.onwheel = function(e) { onCanvasMouseWheel(canvas, e); };
+
+    edges_rad_canvas_1 = edges_rad_max;
+    edges_rad_canvas_2 = edges_rad_max;
+    animate();
+}
+
+
+function resizeCanvas(canvas)
+{
+    xMax = canvas.width  = window.outerWidth / 2.1 + 100;
+    yMax = canvas.height = canvas.offsetHeight * 3;
+    zoom_scale_x = zoom_scale_x * canvas.width / canvas_width;
+    zoom_scale_y = zoom_scale_y * canvas.height  / canvas_height; 
+    edges_rad_max = min(zoom_scale_x, zoom_scale_y) * edge_rad_standart;
+    recordinate_edges();
+}
+window.addEventListener('resize', function(){
+    resizeCanvas(canvas_1);
+    resizeCanvas(canvas_2);
+});
+
+function recordinate_edges(){
+    for (let i = 0; i < edges_positions_canvas_1.length; i++){
+        edges_positions_canvas_1[i].x = edges_positions_canvas_1[i].x * zoom_scale_x;
+        edges_positions_canvas_1[i].y = edges_positions_canvas_1[i].y * zoom_scale_y;
+    }
+    for (let i = 0; i < edges_canvas_2.length; i++){
+        edges_canvas_2[i].x = edges_canvas_2[i].x * zoom_scale_x;
+        edges_canvas_2[i].y = edges_canvas_2[i].y * zoom_scale_y;
+    }
+}
+
+
+function spawn_walls(){
+    var ground = Bodies.rectangle(0, canvas_1.height + 10, 5000, 20, { isStatic: true });
     var wall1 = Bodies.rectangle(-10, 0, 20, 2000, { isStatic: true });
     var wall2 = Bodies.rectangle(0, -10, 5000, 20, { isStatic: true });
-    var wall3 = Bodies.rectangle(canvas.width + 10, 0, 20, 2000, { isStatic: true });
+    var wall3 = Bodies.rectangle(canvas_1.width + 10, 0, 20, 2000, { isStatic: true });
     Composite.add(engine.world, [ground, wall1, wall2, wall3]);
 }
-start();
 
-function drew(ctx_0, x, y){
-    ctx_0.fillStyle = "red";
-    ctx_0.beginPath();
-    ctx_0.arc(x, y, edge_rad, 0, Math.PI * 2)
-    ctx_0.stroke();
-    ctx_0.fill();
+function drew(ctx, point, rad){
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, rad, 0, Math.PI * 2)
+    ctx.stroke();
+    ctx.fill();
 }
 
-function drew_segments(ctx_0, segm){
-    ctx_0.beginPath();
-    ctx_0.moveTo(segm.start.x, segm.start.y);
-    ctx_0.lineTo(segm.end.x, segm.end.y);
-    ctx_0.strokeStyle = "#0bceaf"; 
-    ctx_0.stroke();
+function drew_segments(ctx, segm){
+    ctx.beginPath();
+    ctx.moveTo(segm.start.x, segm.start.y);
+    ctx.lineTo(segm.end.x, segm.end.y);
+    ctx.strokeStyle = "#0bceaf"; 
+    ctx.stroke();
 }
 
-let edges = [];
-engine.world.gravity.y = 0; 
 
 
 function getMousePos(canvas, event) {
@@ -50,12 +118,13 @@ function getMousePos(canvas, event) {
   };
 };
 
-function check_cords_canvas1(cords){
-    if ((cords.x - edge_rad < 0) || (cords.y - edge_rad < 0)){return false;}
-    if ((cords.x + edge_rad > xMax) || (cords.y + edge_rad > yMax)){return false;}
+
+function check_cords(cords, edges, rad){
+    if ((cords.x - rad < 0) || (cords.y - rad < 0)){return false;}
+    if ((cords.x + rad > xMax) || (cords.y + rad > yMax)){return false;}
 
     for (let i = 0; i < edges.length; i++){
-        if (Math.pow(cords.x - edges[i].position.x, 2) + Math.pow(cords.y - edges[i].position.y, 2) < 4 * edge_rad * edge_rad){
+        if (Math.pow(cords.x - edges[i].x, 2) + Math.pow(cords.y - edges[i].y, 2) < 4 * rad * rad){
             return false;
         };
     };
@@ -63,113 +132,88 @@ function check_cords_canvas1(cords){
 };
 
 function new_edge_canvas1(event) {
-    mouse = getMousePos(canvas, event);
-    if (check_cords_canvas1(mouse)){
-        edges.push(Bodies.circle(mouse.x, mouse.y, edge_rad, {
-        inertia: Infinity,
-        frictionStatic: 1,
-        restitution: 1,
-        friction: 0,
-        frictionAir: 0
-    }));
-        Composite.add(engine.world, edges[edges.length - 1]);
+    mouse = getMousePos(canvas_1, event);
+    if (check_cords(mouse, edges_positions_canvas_1, edges_rad_canvas_1)){
+        let body =  Bodies.circle(mouse.x, 
+                                mouse.y, 
+                                edges_rad_canvas_1, 
+                                {
+                                    inertia: Infinity,
+                                    frictionStatic: 1,
+                                    restitution: 1,
+                                    frictionAir: 0,
+                                    friction: 0,
+                                });
+        edges_bodies_canvas_1.push(body);
+        Composite.add(engine.world, body);
         let angel = Math.random() * 2 * Math.PI;
-        Matter.Body.setVelocity(edges[edges.length - 1], {x: speed * Math.cos(angel), y: speed * Math.sin(angel),})
+        Matter.Body.setVelocity(body, {x: speed * Math.cos(angel), y: speed * Math.sin(angel),});
+        edges_positions_canvas_1.push(body.position);
     };
 };
 
-function drew_edges_canvas1(){
+function new_edge_canvas2(event) {
+    mouse = getMousePos(canvas_2, event);
+    if (check_cords(mouse, edges_canvas_2, edges_rad_canvas_2)){
+        edges_canvas_2.push({x:mouse.x, y:mouse.y});
+        drew(ctx_2, mouse, edges_rad_canvas_2)
+    };
+};
+
+
+function drew_edges(ctx, edges, rad){
     for (let i = 0; i < edges.length; i++){
-        drew(ctx, edges[i].position.x, edges[i].position.y);
+        drew(ctx, edges[i], rad);
     };
 };
 
 function clear_edges_canvas1(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx_1.clearRect(0, 0, canvas_1.width, canvas_1.height);
     engine = Engine.create();
-    start()
+    spawn_walls()
     engine.world.gravity.y = 0; 
-    edges = [];
+    edges_positions_canvas_1 = [];
+    edges_bodies_canvas_1 = [];
+}
+
+function clear_edges_canvas_2(){
+    ctx_2.clearRect(0, 0, canvas_2.width, canvas_2.height);
+    edges_canvas_2 = [];
 }
 
 function voronoi_canvas_1(){
-    if (edges.length < 2){return;}
-    let points =[]
-    for (let i = 0; i < edges.length; i++){points.push(edges[i].position);}
-    let vor = new VoronoiDiagram(points, canvas.width, canvas.height); 
+    if (edges_positions_canvas_1.length < 2){return;}
+    let vor = new VoronoiDiagram(edges_positions_canvas_1, canvas_1.width, canvas_1.height); 
     vor.update();
     let segments = vor.edges;
     for (let i = 0; i < segments.length; i++){
         if (segments[i] != null){
-            drew_segments(ctx, segments[i]);
+            drew_segments(ctx_1, segments[i]);
         }
     }
 }
 
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drew_edges_canvas1();
+    ctx_1.clearRect(0, 0, canvas_1.width, canvas_1.height);
+    drew_edges(ctx_1, edges_positions_canvas_1, edges_rad_canvas_1);
     if (voronoi_on){
         voronoi_canvas_1();
     }
     requestAnimationFrame(animate);
-    Engine.update(engine, 1000 / 60);
+    Engine.update(engine, render);
 }
-animate();
 
 
 function turn_voronoi(){
     voronoi_on = !voronoi_on;
 }
 
-//canvas 2
-
-const canvas_2 = document.getElementById("canvas_2");
-const ctx_2 = canvas_2.getContext("2d");
-let edge_rad_canvas2 = 10; 
-canvas_2.width = window.innerWidth * 0.55;
-canvas_2.height = window.innerHeight * 0.65;
-
-let edges_2 = [] 
-
-function check_cords_canvas_2(cords){
-    if ((cords.x - edge_rad_canvas2 < 0) || (cords.y - edge_rad_canvas2 < 0)){return false;}
-    if ((cords.x + edge_rad_canvas2 > xMax) || (cords.y + edge_rad_canvas2 > yMax)){return false;}
-
-    for (let i = 0; i < edges_2.length; i++){
-        if (Math.pow(cords.x - edges_2[i].x, 2) + Math.pow(cords.y - edges_2[i].y, 2) < 4 * edge_rad * edge_rad){
-            return false;
-        };
-    };
-    return true;
-};
-
-
-function new_edge_canvas2(event) {
-    mouse = getMousePos(canvas_2, event);
-    if (check_cords_canvas_2(mouse)){
-        edges_2.push({x:mouse.x, y:mouse.y});
-        drew(ctx_2, mouse.x, mouse.y)
-    };
-};
-
-function drew_edges_canvas_2(){
-    for (let i = 0; i < edges_2.length; i++){
-        drew(ctx_2, edges_2[i].x, edges_2[i].y);
-    };
-}
-
-
-function clear_edges_canvas_2(){
-    ctx_2.clearRect(0, 0, canvas.width, canvas.height);
-    edges_2 = [];
-}
 
 function voronoi(){
-    ctx_2.clearRect(0, 0, canvas.width, canvas.height);
-    drew_edges_canvas_2();
-    if (edges_2.length < 2){return;}
-    let vor = new VoronoiDiagram(edges_2, canvas_2.width, canvas_2.height); 
+    ctx_2.clearRect(0, 0, canvas_2.width, canvas_2.height);
+    drew_edges(ctx_2, edges_canvas_2, edges_rad_canvas_2);
+    if (edges_canvas_2.length < 2){return;}
+    let vor = new VoronoiDiagram(edges_canvas_2, canvas_2.width, canvas_2.height); 
     vor.update();
     let segments = vor.edges;
     for (let i = 0; i < segments.length; i++){
@@ -179,10 +223,3 @@ function voronoi(){
     }
 
 }
-
-window.addEventListener('resize', function(){
-    xMax = canvas.width = canvas_2.width = window.innerWidth * 0.55;
-    yMax = canvas.height = canvas_2.height = window.innerHeight * 0.65;
-    drew_edges_canvas1();
-    drew_edges_canvas_2();
-});
